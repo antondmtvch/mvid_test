de49cb78cdc3f80d5477ed4eaf286cc12160475b
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-21 23:45:07 +0300

    Added config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+
+class BaseConfig:
+    DEBUG = False
+
+    APP_HOST = '127.0.0.1'
+    APP_PORT = 8080
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATASET_PATH = os.path.join(ROOT_DIR, 'data/recommends.csv')
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = False
