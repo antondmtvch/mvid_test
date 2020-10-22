be2a92d2edebc7abbc0c35e84fb962cbe78b2d9b
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-22 11:54:03 +0300

    Moved config on root project dir

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class BaseConfig:
     APP_HOST = '127.0.0.1'
     APP_PORT = 8080
 
-    ROOT_DIR = Path(__file__).resolve().parent.parent
+    ROOT_DIR = Path(__file__).resolve().parent
     DATASET_PATH = os.path.join(ROOT_DIR, 'data/recommends.csv')
 
 
