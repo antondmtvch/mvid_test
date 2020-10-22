8a915cfc7180a13a9e9ebd023a1bd659c6427c6c
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-22 14:53:46 +0300

    Updated boot.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 app="mvid"
 docker build -t ${app} .
-docker run -d -p 8080:8080 --name=${app} -v $PWD:/app ${app}
+docker run -d -p 5000:80 --name=${app} -v $PWD/data:/var/www/mvid/data ${app}
